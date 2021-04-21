f5968a3fa8f11cda542e53c61f2fb360dd5c090d
deeptronos <ecnemec1132@gmail.com>
2021-04-20 20:47:00 -0500

    Began writing operator>>

--- cities.cc
+++ cities.cc
@@ -1,3 +1,12 @@
 #include "cities.hh"
-
+//What's the difference between #include-ing iostream and istream/ostream separately?
 #include <iostream>
+#include <istream>
+#include <ostream
+
+//allow for the reading  / filling of a Cities object from a stream. Choose whether this needs to be a method or a free function.
+//When implemented, we should be able to run code like:
+	// std::cin >> cities;
+std::istream& operator>>(std::istream&, Cities&){ // Read:
+
+}
