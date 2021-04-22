0a2bf1619b6161fd4a616a69b7b156116fa90c40
deeptronos <ecnemec1132@gmail.com>
2021-04-21 20:55:31 -0500

    Worked on operator>>

--- cities.cc
+++ cities.cc
@@ -4,9 +4,71 @@
 #include <istream>
 #include <ostream
 
+#include <string> // std::stoi
+
 //allow for the reading  / filling of a Cities object from a stream. Choose whether this needs to be a method or a free function.
 //When implemented, we should be able to run code like:
 	// std::cin >> cities;
-std::istream& operator>>(std::istream&, Cities&){ // Read:
+std::istream& operator>>(std::istream& iS, Cities& c){ // Read: a .tsv file, containing rows formatted as "int int", to populate a vector of pairs (i.e., a cityCollection_t)
+	if(iS == nullptr){ // Check if iS is a valid istream
+		return nullptr;
+	}
+
+	Cities::cityCollection_t cityCollection;
+	char in;
+	iS.get(in); // Read the first char from the stream
+
+	while(in != eof()) {
+
+		int pair[2] = -1, -1;
+
+		for (int i = 0; i <= 1; i++) {
+			if(in == eof()){break;} // If we encounter an EOF, break
+
+			std::string numStr;
+
+			while (true) { //Loop until break
+				if (in == ' ' || in == of() ||
+				    in == '\n') { break } // If we encounter a space, an EOF, or a newline, break
+
+				numStr += in; // add in to numStr
+				iS.get(in); // Get next char
+			}
+			int num = std::stoi(numStr); // Convert numStr to an int
+			pair[i] = num; // Set the i-th entry in pair to our num
+		} // now, pair contains the integers for a coord_t!
+		if(pair[0] == -1 || pair[1] == -1){break;} // if pair is unchanged from its initialization, break
+
+		Cities::coord_t newCity(pair[0], pair[1]); // make a new coord_t based upon pair
+		cityCollection.push_back(newCity); // Add newCity to cityCollection
+	}
+
+//	while(true){ //Loop will run until we break from it
+//		if(in == eof()) { break; }// If we've just read in an EOF character, break!
+//		else{
+//			int num;
+//			std::string numStr;
+//
+//			while( in != ' ' && in != '\n'){ // Get char from stream and store it in char in, until we reach a space char (' ') or a newline
+//				numStr += in; // add char in to numStr
+//				iS.get(in); // Read a char from the stream
+//			}
+//			if(in == ' '){ // If we've read in a space character...
+//				while(in == ' '){ // Read from the stream until we read a non-space character
+//					iS.get(in);
+//				}
+//			}else if(in == '\n') { // If we've read in a newline...
+//
+//			}
+//		}
+//
+//	}
+
+
+
+
+	if(iS>>i){ // Did we succeed at reading an int into i?
+
+	}
 
 }
--- cities.hh
+++ cities.hh
@@ -13,9 +13,13 @@ class Cities {
   // A pair of integral coordinates for each city
   using coord_t = std::pair<int, int>;
 
-  // An ordering of the cities in cities_t. Each value represents a unique index
+  // An ordering of cities. Each value represents a unique index
   // into the current city ordering.
   using permutation_t = std::vector<unsigned int>;
+
+  // The data type used to contain the cities in a Cities object.
+  // Each coord_t entry in the vector represents a city.
+  using cityCollection_t = std::vector<coord_t>;
 
   // Given a permutation, return a new Cities object where the order of the
   // cities reflects the original order of this class after reordering with
@@ -26,7 +30,14 @@ class Cities {
   // For a given permutation of the cities in this object,
   // compute how long (distance) it would take to traverse all the cities in the
   // order of the permutation, and then returning to the first city.
-  // The distance between any two cities is computed as the Euclidean 
+  // The distance between any two cities is computed as the Euclidean
   // distance on a plane between their coordinates.
   double total_path_distance(const permutation_t& ordering) const;
+
+  //return the cities in this Cities object
+  cityCollection_t get_cities(){return myCities;}
+
+ private:
+	// A vector containing the coord_t's of each city in the object.
+	cityCollection_t myCities;
 };
